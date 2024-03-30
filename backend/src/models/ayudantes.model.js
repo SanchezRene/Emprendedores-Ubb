50d3703b7326d57805482e9f816d4aa4d309bfca
ReneSanchezH <rene.sanchez2001@alumnos.ubiobio.cl>
2024-03-30 15:21:09 -0300

    Ayudantes/colaboradores que trabajan en el puesto

---
+++ backend/src/models/ayudantes.model.js
@@ -0,0 +1,22 @@
+"use strict";
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Cada Emprendimiento tiene un arreglo de Ayudantes. Los ayudantes pueden ser amigos o parientes del emprendedor, por lo que pueden o no ser de la universidad.
+
+const ayudantesSchema = new mongoose.Schema({
+
+  nombre: {
+    type: String,
+    required: true,
+    maxLenght: 100,
+  },
+  rut: {
+    type: String,
+    required: true,
+  },
+});
+
+const Ayudantes = mongoose.model("Ayudantes", ayudantesSchema);
+
+module.exports = Ayudantes;
