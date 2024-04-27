24e024ede7c8ffec571747e8c601d701afc0c80f
ReneSanchezH <rene.sanchez2001@alumnos.ubiobio.cl>
2024-04-27 17:55:32 -0400

    modificación del modelo de inscripciones

--- backend/src/controllers/inscripcion.controller.js
+++ backend/src/controllers/inscripcion.controller.js
@@ -1,119 +1,118 @@
-'use strict';
+"use strict";
 
 const { respondSuccess, respondError } = require("../utils/resHandler");
 const InscripcionService = require("../services/inscripcion.service");
 const EmprendedorService = require("../services/emprendedor.service");
 const InscripcionSchema = require("../schema/inscripcion.schema");
+const ProductosService = require("../services/productos.service");
 const { handleError } = require("../utils/errorHandler");
 
 async function getInscripcionesSummary(req, res) {
-    try {
-        const [inscripciones, errorInscripciones] = await InscripcionService.getInscripcionesSummary();
-        if (errorInscripciones) return respondError(req, res, 404, errorInscripciones);
+  try {
+    const [inscripciones, errorInscripciones] =
+      await InscripcionService.getInscripcionesSummary();
+    if (errorInscripciones)
+      return respondError(req, res, 404, errorInscripciones);
 
-        inscripciones.length === 0
-            ? respondSuccess(req, res, 204)
-            : respondSuccess(req, res, 200, inscripciones);
-    } catch (error) {
-        handleError(error, "inscripcion.controller -> getInscripcionesSummary");
-        respondError(req, res, 400, error.message);
-    }
+    inscripciones.length === 0
+      ? respondSuccess(req, res, 204)
+      : respondSuccess(req, res, 200, inscripciones);
+  } catch (error) {
+    handleError(error, "inscripcion.controller -> getInscripcionesSummary");
+    respondError(req, res, 400, error.message);
+  }
 }
 
 async function getInscripcionById(req, res) {
-    try {
-        const { params } = req;
-        const { error: paramsError } =
-            InscripcionSchema.inscripcionIdSchema.validate(params);
-        if (paramsError) return respondError(req, res, 400, paramsError.message);
+  try {
+    const { params } = req;
+    const { error: paramsError } =
+      InscripcionSchema.inscripcionIdSchema.validate(params);
+    if (paramsError) return respondError(req, res, 400, paramsError.message);
 
-        const [inscripcion, errorInscripcion] = await InscripcionService.getInscripcionById(
-            params.id,
-        );
-        if (errorInscripcion) return respondError(req, res, 404, errorInscripcion);
+    const [inscripcion, errorInscripcion] =
+      await InscripcionService.getInscripcionById(params.id);
+    if (errorInscripcion) return respondError(req, res, 404, errorInscripcion);
 
-        respondSuccess(req, res, 200, inscripcion);
-    } catch (error) {
-        handleError(error, "inscripcion.controller -> getInscripcionById");
-        respondError(req, res, 400, error.message);
-    }
+    respondSuccess(req, res, 200, inscripcion);
+  } catch (error) {
+    handleError(error, "inscripcion.controller -> getInscripcionById");
+    respondError(req, res, 400, error.message);
+  }
 }
 
 async function createInscripcion(req, res) {
-    try {
-        const { body } = req;
+  try {
+    const { body } = req;
 
-        const [productos, errorProductos] = await EmprendedorService.getProductosByEmprendedor(body.emprendedorId);
-        if (errorProductos) return respondError(req, res, 404, errorProductos);
+    const { error: bodyError } =
+      InscripcionSchema.inscripcionBodySchema.validate(body);
+    if (bodyError) return respondError(req, res, 400, bodyError.message);
 
-        const productosId = productos.map((productos) => productos._id.toString());
+    const [inscripcion, errorInscripcion] =
+      await InscripcionService.createInscripcion(body);
 
-        const RequestBody = {
-            ...body,
-            productosId: productosId
-        };
+    if (errorInscripcion) return respondError(req, res, 404, errorInscripcion);
+    if (!inscripcion)
+      return respondError(req, res, 400, "No se creó la inscripción");
 
-        const { error: bodyError } =
-            InscripcionSchema.inscripcionBodySchema.validate(RequestBody );
-        if (bodyError) return respondError(req, res, 400, bodyError.message);
-
-        const [inscripcion, errorInscripcion] = await InscripcionService.createInscripcion(RequestBody);
-        if (errorInscripcion) return respondError(req, res, 404, errorInscripcion);
-
-        respondSuccess(req, res, 201, inscripcion);
-    } catch (error) {
-        handleError(error, "inscripcion.controller -> createInscripcion");
-        respondError(req, res, 400, error.message);
-    }
+    respondSuccess(req, res, 201, inscripcion);
+  } catch (error) {
+    handleError(error, "inscripcion.controller -> createInscripcion");
+    respondError(req, res, 400, error.message);
+  }
 }
 
 async function updateInscripcion(req, res) {
-    try {
-        const { body, params } = req;
-        const { error: bodyError } =
-            InscripcionSchema.inscripcionBodySchema.validate(body);
-        if (bodyError) return respondError(req, res, 400, bodyError.message);
+  try {
+    const { body, params } = req;
+    const { error: bodyError } =
+      InscripcionSchema.inscripcionBodySchema.validate(body);
+    if (bodyError) return respondError(req, res, 400, bodyError.message);
 
-        const { error: paramsError } =
-            InscripcionSchema.inscripcionIdSchema.validate(params);
-        if (paramsError) return respondError(req, res, 400, paramsError.message);
+    const { error: paramsError } =
+      InscripcionSchema.inscripcionIdSchema.validate(params);
+    if (paramsError) return respondError(req, res, 400, paramsError.message);
 
-        const [inscripcion, errorInscripcion] = await InscripcionService.updateInscripcion(
-            params.id,
-            body,
-        );
-        if (errorInscripcion) return respondError(req, res, 404, errorInscripcion);
+    const [inscripcion, errorInscripcion] =
+      await InscripcionService.updateInscripcion(params.id, body);
+    if (errorInscripcion) return respondError(req, res, 404, errorInscripcion);
 
-        respondSuccess(req, res, 200, inscripcion);
-    } catch (error) {
-        handleError(error, "inscripcion.controller -> updateInscripcion");
-        respondError(req, res, 400, error.message);
-    }
+    respondSuccess(req, res, 200, inscripcion);
+  } catch (error) {
+    handleError(error, "inscripcion.controller -> updateInscripcion");
+    respondError(req, res, 400, error.message);
+  }
 }
 
 async function deleteInscripcion(req, res) {
-    try {
-        const { params } = req;
-        const { error: paramsError } =
-            InscripcionSchema.inscripcionIdSchema.validate(params);
-        if (paramsError) return respondError(req, res, 400, paramsError.message);
+  try {
+    const { params } = req;
+    const { error: paramsError } =
+      InscripcionSchema.inscripcionIdSchema.validate(params);
+    if (paramsError) return respondError(req, res, 400, paramsError.message);
 
-        const [inscripcion, errorInscripcion] = await InscripcionService.deleteInscripcion(
-            params.id,
-        );
-        if (errorInscripcion) return respondError(req, res, 404, errorInscripcion);
+    const [inscripcion, errorInscripcion] =
+      await InscripcionService.deleteInscripcion(params.id);
+    if (errorInscripcion) return respondError(req, res, 404, errorInscripcion);
 
-        respondSuccess(req, res, 200,"Inscripción eliminada satisfactoriamente", inscripcion);
-    } catch (error) {
-        handleError(error, "inscripcion.controller -> deleteInscripcion");
-        respondError(req, res, 400, error.message);
-    }
+    respondSuccess(
+      req,
+      res,
+      200,
+      "Inscripción eliminada satisfactoriamente",
+      inscripcion,
+    );
+  } catch (error) {
+    handleError(error, "inscripcion.controller -> deleteInscripcion");
+    respondError(req, res, 400, error.message);
+  }
 }
 
 module.exports = {
-    getInscripcionesSummary,
-    getInscripcionById,
-    createInscripcion,
-    updateInscripcion,
-    deleteInscripcion,
+  getInscripcionesSummary,
+  getInscripcionById,
+  createInscripcion,
+  updateInscripcion,
+  deleteInscripcion,
 };
